d10930e7adfcf90a16287bf97e2a7444fc3574cb
Alessandro Freda <61095516+alefreda@users.noreply.github.com>
2020-11-12 19:45:14 +0100

    Update main.py
    Estrarre errori, numero di run, testo incollato

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import dateutil.parser
 
 '''
-data[0]["time"]   -> apertura
-data[-1]["time"]   -> chiusura
+logs[0]["time"]   -> apertura
+logs[-1]["time"]   -> chiusura
 
 
 "sequence" "ShellCommand" "command_text" "%Run" -> Run
@@ -14,26 +14,67 @@ sequence" "<<Paste>>" "text_widget_class" "CodeViewText" -> paste
 
 '''
 
+#data
+def printDate(logs):
 
-def printDate(data):
-    print("Apertura: ", data[0]["time"])
-    print("Chiusura: ", data[-1]["time"])
+    print("Apertura: ", logs[0]["time"])
+    print("Chiusura: ", logs[-1]["time"])
     #datetime
-    print(dateutil.parser.parse(data[0]["time"]))
- 
+    print(dateutil.parser.parse(logs[0]["time"]))
 
+#errori
+def printErrors(logs):
+
+    for i in range(len(logs)):
+        if "text" in logs[i]:
+            #check se la stringa error è nel valore corrispondente
+            if "Error" in logs[i]["text"]:
+                print(logs[i]["text"],"at time: ", dateutil.parser.parse(logs[i]["time"]))
+            else:
+                pass
+        else:
+            pass
+#testo incollato
+def printPastedText(logs):
+    for i in range(len(logs)):
+        if "sequence" in logs[i]:
+            #check se la stringa paste è nel valore corrispondente
+            if "<<Paste>>" in logs[i]["sequence"]:
+                #il testo si trova in i-1 in "text"
+                print(logs[i-1]["text"],"at time: ",dateutil.parser.parse(logs[i]["time"]))
+            else:
+                pass
+        else:
+            pass
+#run
+def printRun(logs):
+    run = 0
+    for i in range(len(logs)):
+        if "command_text" in logs[i]:
+            #check se la stringa run è nel valore corrispondente
+            if "%Run" in logs[i]["command_text"]:
+                run = logs[i]["command_text"]
+                print(run, "at time:", dateutil.parser.parse(logs[i]["time"]))
+            else:
+                pass
+        else:
+            pass
+    print("numero di run: ", len(run))
 
 def main():
-
+    logs = 0
     try:
         f = open("logs/5/2020-04-16_14-10-53_0.txt")
-        data = json.load(f)
+        logs = json.load(f)
         f.close()
     except Exception:
         print('Errore nella lettura file: ', Exception)
     
-    printDate(data) 
-    
+    printDate(logs) 
+    printErrors(logs)
+    printPastedText(logs)
+    printRun(logs)
+
 
 
 if __name__ == "__main__":
