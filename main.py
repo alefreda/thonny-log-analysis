091d7f6243758c882f3e539815ae9f52d537a106
Alessandro Freda <61095516+alefreda@users.noreply.github.com>
2020-11-12 10:34:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import pandas as pd
+import dateutil.parser
 
 '''
 data[0]["time"]   -> apertura
@@ -13,23 +15,24 @@ sequence" "<<Paste>>" "text_widget_class" "CodeViewText" -> paste
 '''
 
 
+def printDate(data):
+    print("Apertura: ", data[0]["time"])
+    print("Chiusura: ", data[-1]["time"])
+    #datetime
+    print(dateutil.parser.parse(data[0]["time"]))
+ 
+
+
 def main():
 
     try:
         f = open("logs/5/2020-04-16_14-10-53_0.txt")
-        data = json.load(f) 
+        data = json.load(f)
         f.close()
-
-        print("Apertura: ", data[0]["time"])
-        print("Chiusura: ", data[-1]["time"])
-
-
     except Exception:
-        print('Errore nella lettura file')
+        print('Errore nella lettura file: ', Exception)
     
-
-    
-
+    printDate(data) 
     
 
 
